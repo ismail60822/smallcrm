0bb16a94bdce3d428c81ceedac7ad699cb13ff8e
ismailfsd2 <ismailfsd2@gmail.com>
2021-01-15 19:26:41 +0500

    database connection change

--- admin/dbconnection.php
+++ admin/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "crm");
+$con=mysqli_connect("ismailfsd.com", "ismailfsd_smallcrm_us", "Testing123#@!", "ismailfsd_smallcrm");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
