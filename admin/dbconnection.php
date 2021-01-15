5f114a422117fa4091f3a6989aa39e5c2d2dbdce
ismailfsd2 <ismailfsd2@gmail.com>
2021-01-15 19:42:40 +0500

    Change database connection

--- admin/dbconnection.php
+++ admin/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("ismailfsd.com", "ismailfsd_smallcrm_us", "Testing123#@!", "ismailfsd_smallcrm");
+$con=mysqli_connect("sql12.freemysqlhosting.net", "sql12387183", "nE89zd5HZk", "sql12387183");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
